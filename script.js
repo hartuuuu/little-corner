f1ffb98e2c6460600d95d16ee776eecd961d7c2b
hartuuuu <animeloverjannacorro@gmail.com>
2026-08-04 15:21:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const category = document.querySelector(".category");
 const message = document.querySelector(".message");
 const button = document.getElementById("randomBtn");
+const content = document.getElementById("content");
+
+const buttonTexts = [
+
+"Explore",
+
+"Again",
+
+"Another one",
+
+"Keep going",
+
+"Surprise me",
+
+"One more",
+
+"What's next?"
+
+];
 
 let lastIndex = -1;
 
@@ -16,10 +35,15 @@ function showCard() {
 
     const card = cards[index];
 
-    category.textContent = `${card.icon} ${card.title}`;
+category.textContent = `${card.icon} ${card.title}`;
 
-    message.innerHTML = card.text.replace(/\n/g, "<br>");
+message.innerHTML = card.text.replace(/\n/g, "<br>");
+
+content.classList.remove("fade");
+void content.offsetWidth;
+content.classList.add("fade");
+
+button.textContent =
+buttonTexts[Math.floor(Math.random() * buttonTexts.length)];
 
 }
-
-button.addEventListener("click", showCard);
